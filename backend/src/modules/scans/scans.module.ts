1ca57041ee21db5c53700be69b8b26a3b969b00f
gitavenian <gitaravenian@gmail.com>
2026-05-08 18:09:59 +0300

    some changes

--- backend/src/modules/scans/scans.module.ts
+++ backend/src/modules/scans/scans.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ScansService } from './scans.service';
 import { ScansController } from './scans.controller';
+import { Scan, ScanSchema } from './schemas/scan.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Scan.name, schema: ScanSchema }]),
+  ],
   controllers: [ScansController],
   providers: [ScansService],
+  exports: [ScansService],
 })
 export class ScansModule {}
